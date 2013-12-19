bd3e82f4141a435cf444c67a1016a9d42e0b89c0
fclemens <fclemens@CLS-CLEMENS.pc.cls.fr>
2013-12-19 11:17:26 +0100

    add parser management

--- Assa.py
+++ Assa.py
@@ -4,6 +4,13 @@ Created on 24 sept. 2013
 @author: awikatchikaen
 '''
 
+from parsers.LBC.LBCparser import LBCSearchResultsParser
+from parsers.ParsersManager import ParsersManager
+
+
 if __name__ == '__main__':
-    print("test")
+    print("init assa")
+    parserManager = ParsersManager()
+    parserManager.registerParser("LBC",LBCSearchResultsParser())
+    parserManager.getListParsers()
     pass
